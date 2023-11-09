2793fd3d0f61be952e77ec112d5af8b7473cdf01
vaishnaviAnupindi <nagavaishnavi.anupindi@apoorva.com>
2023-11-10 04:42:28 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,18 @@
+/*
+<div id = "parent">
+    <div id = "child">
+        <h1>I am an h1 tag</h1>
+    </div>
+</div>
+
+*/
+
+const parent = React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child"},React.createElement("h1",{},"I am an h1 tag")
+)
+);
+
+// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React");
+console.log(parent); 
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
